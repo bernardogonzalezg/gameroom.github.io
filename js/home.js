2e6966ff033436e3e85fc639dc1c49a45434f9fe
Bernardo Gonzalez <bgonzalezgirodo@alumnos.exa.unicen.edu.ar>
2022-10-05 17:12:57 -0300

    menues desplegables

--- js/home.js
+++ js/home.js
@@ -131,3 +131,15 @@ puntoTres.forEach( (cadaPunto, i) => {
     });
     
 });
+
+document.querySelector(".burger").addEventListener("click", mostrarMenu);
+
+function mostrarMenu() {
+    document.querySelector(".menu").classList.toggle("show");
+}
+
+document.querySelector(".btnUser").addEventListener("click", mostrarMenuUsuario);
+
+function mostrarMenuUsuario() {
+    document.querySelector(".user").classList.toggle("showUser");
+}
