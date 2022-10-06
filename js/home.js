9b025bec1a81f429bcbd5b195999c685b991367c
juan manuel lahorca <juanmanuellahorca@gmail.com>
2022-10-06 11:20:13 -0300

    responsive carrusel de a 1

--- js/home.js
+++ js/home.js
@@ -1,7 +1,7 @@
 "use strict";
 
 //document.querySelector(".burguer").addEventListener("click", mostrarMenu);
-document.querySelector("#btn-comprar-Circus").addEventListener("click", GamebuyCircus);   
+document.querySelector("#btn-comprar-Circus").addEventListener("click", GamebuyCircus);    
 document.querySelector("#btn-comprar-Tetris").addEventListener("click", GamebuyTetris);  
 document.querySelector("#btn-comprar-Galaga").addEventListener("click", GamebuyGalaga);  
 document.querySelector("#btn-comprar-Truco").addEventListener("click", GamebuyTruco);
@@ -11,9 +11,108 @@ document.querySelector("#btn-comprar-Goal3").addEventListener("click", GamebuyGo
 document.querySelector("#btn-comprar-MicroMachine").addEventListener("click", GamebuyMicromachine);
 document.querySelector("#btn-comprar-PingPong").addEventListener("click", GamebuyPingpong);
 
+document.querySelector("#btn-comprar-Circus-resp").addEventListener("click", GamebuyCircusResp);
+document.querySelector("#btn-comprar-Tetris-resp").addEventListener("click", GamebuyTetrisResp);
+document.querySelector("#btn-comprar-Galaga-resp").addEventListener("click", GamebuyGalagaResp);
+document.querySelector("#btn-comprar-Truco-resp").addEventListener("click", GamebuyTrucoResp);
+document.querySelector("#btn-comprar-Tute-resp").addEventListener("click", GamebuyTuteResp);
+document.querySelector("#btn-comprar-Uno-resp").addEventListener("click", GamebuyUnoResp);
+document.querySelector("#btn-comprar-Goal3-resp").addEventListener("click", GamebuyGoal3Resp);
+document.querySelector("#btn-comprar-MicroMachine-resp").addEventListener("click", GamebuyMicroMachineResp);
+document.querySelector("#btn-comprar-PingPong-resp").addEventListener("click", GamebuyPingPongResp);
+
 function mostrarMenu() {
     document.querySelector(".menu").classList.toggle("show");
 }
+
+function GamebuyCircusResp(){
+    let carrito = document.querySelector("#cart-inImg-CircusResp");
+    console.log(carrito);
+    let string = "fa-solid fa-cart-arrow-down";
+    carrito.innerHTML = `<i class="${string}"></i>`;
+}
+
+function GamebuyTetrisResp(){
+    let carrito = document.querySelector("#cart-inImg-TetrisResp");
+    console.log(carrito);
+    let string = "fa-solid fa-cart-arrow-down";
+    carrito.innerHTML = `<i class="${string}"></i>`;
+}
+function GamebuyGalagaResp(){
+    let carrito = document.querySelector("#cart-inImg-GalagaResp");
+    console.log(carrito);
+    let string = "fa-solid fa-cart-arrow-down";
+    carrito.innerHTML = `<i class="${string}"></i>`;
+}
+function GamebuyTrucoResp(){
+    let carrito = document.querySelector("#cart-inImg-TrucoResp");
+    console.log(carrito);
+    let string = "fa-solid fa-cart-arrow-down";
+    carrito.innerHTML = `<i class="${string}"></i>`;
+}
+function GamebuyTuteResp(){
+    let carrito = document.querySelector("#cart-inImg-TuteResp");
+    console.log(carrito);
+    let string = "fa-solid fa-cart-arrow-down";
+    carrito.innerHTML = `<i class="${string}"></i>`;
+}
+function GamebuyUnoResp(){
+    let carrito = document.querySelector("#cart-inImg-UnoResp");
+    console.log(carrito);
+    let string = "fa-solid fa-cart-arrow-down";
+    carrito.innerHTML = `<i class="${string}"></i>`;
+}
+function GamebuyGoal3Resp(){
+    let carrito = document.querySelector("#cart-inImg-Goal3Resp");
+    console.log(carrito);
+    let string = "fa-solid fa-cart-arrow-down";
+    carrito.innerHTML = `<i class="${string}"></i>`;
+}
+function GamebuyMicroMachineResp(){
+    let carrito = document.querySelector("#cart-inImg-MicroMachineResp");
+    console.log(carrito);
+    let string = "fa-solid fa-cart-arrow-down";
+    carrito.innerHTML = `<i class="${string}"></i>`;
+}
+function GamebuyPingPongResp(){
+    let carrito = document.querySelector("#cart-inImg-PingPongResp");
+    console.log(carrito);
+    let string = "fa-solid fa-cart-arrow-down";
+    carrito.innerHTML = `<i class="${string}"></i>`;
+}
+
+/*  INTENTO DE REUTILIZAR CODIGO
+
+
+function btnNormal(evento){
+    let buttons = document.querySelectorAll('.btn-comprar');
+    for(let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', obtenerId(evento));
+    }
+    
+};
+
+function btnReponsive(evento){
+     let buttonsResp = document.querySelectorAll('.btn-comprar-resp');
+     for(let i = 0; i < buttons.length; i++) {
+         buttonsResp[i].addEventListener('click', obtenerId);
+    }
+}
+
+function obtenerId(evento) {
+        console.log(evento);
+        console.log(evento.target.id);
+        Gamebuy(evento.target.id);  
+    };  
+
+function Gamebuy( param ){
+    console.log(param);
+    let carrito = document.querySelector(`#cart-inImg-${param}`);
+    console.log(carrito);
+    let string = "fa-solid fa-cart-arrow-down";
+    carrito.innerHTML = `<i class="${string}"></i>`;
+}*/
+
 
 function GamebuyCircus(){
     let carrito = document.querySelector("#cart-inImg-Circus");
@@ -79,6 +178,7 @@ function GamebuyPingpong(){
 }
 
 
+
 //CARRUSELES
 
 const caja = document.querySelector(".caja-carrusel");
@@ -131,6 +231,62 @@ puntoTres.forEach( (cadaPunto, i) => {
     });
     
 });
+
+// CARRUSEL RESPONSIVE
+
+const cajaResp = document.querySelector(".caja-carrusel-resp");
+const puntoResp = document.querySelectorAll(".punto-resp");
+
+puntoResp.forEach( (cadaPunto, i) => {
+    puntoResp[i].addEventListener("click", () => {
+        let posicion = i;
+        let operacion = posicion * -15;
+        cajaResp.style.transform = `translateX(${ operacion }%)`;
+
+        puntoResp.forEach( (cadaPunto, i) =>{
+            puntoResp[i].classList.remove("activo-resp");
+        });
+        puntoResp[i].classList.add("activo-resp");
+    });
+    
+});
+
+const cajaDosResp = document.querySelector(".caja-carrusel-dos-resp");
+const puntoDosResp = document.querySelectorAll(".punto-dos-resp");
+
+puntoDosResp.forEach( (cadaPunto, i) => {
+    puntoDosResp[i].addEventListener("click", () => {
+        let posicion = i;
+        let operacion = posicion * -15;
+        cajaDosResp.style.transform = `translateX(${ operacion }%)`;
+
+        puntoDosResp.forEach( (cadaPunto, i) =>{
+            puntoDosResp[i].classList.remove("activo-resp");
+        })
+        puntoDosResp[i].classList.add("activo-resp");
+    });
+    
+});
+
+const cajaTresResp = document.querySelector(".caja-carrusel-tres-resp");
+const puntoTresResp = document.querySelectorAll(".punto-tres-resp");
+
+puntoTresResp.forEach( (cadaPunto, i) => {
+    puntoTresResp[i].addEventListener("click", () => {
+        let posicion = i;
+        let operacion = posicion * -15;
+        cajaTresResp.style.transform = `translateX(${ operacion }%)`;
+
+        puntoTresResp.forEach( (cadaPunto, i) =>{
+            puntoTresResp[i].classList.remove("activo-resp");
+        })
+        puntoTresResp[i].classList.add("activo-resp");
+    });
+    
+});
+
+
+// MUNES DESPLEGBLES
 
 document.querySelector(".burger").addEventListener("click", mostrarMenu);
 
